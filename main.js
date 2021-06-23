2db73ae21a1886dbb67b4d39b51cec55614a453d
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-06-23 13:11:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ value = 'str'
 value = null
 value = false
 console.log(typeof value) 
+
+ ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+
+let str = '123'
+let num = Number(str)
+let object = String('asd')
+console.log(typeof num)
+console.log(typeof object)
